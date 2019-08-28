a5c7a42497a90978bb79b0a69f5e5ab330d85c21
abryantr <54371404+abryantr@users.noreply.github.com>
2019-08-27 23:52:25 -0400

    fixed double-counting duplicates
    the Jedi part of the assignment was double counting duplicate entries as distinct purchases.
    rebuilt code to make better use of arrays to check for double counting

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,87 +8,101 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		//reads input describing number of products being sold
+		//reads number of products from input
 		int number_of_products = scan.nextInt();
 		
-		//these arrays function together as a matrix
-		//this array will contain the names of each product
+		//array holding the names of each product
 		String[] product_names = new String[number_of_products];
-		//this array will contain the number of each product purchased
-		int[] number_purchases = new int[number_of_products];
-		//this array will contain the number of customers who purchased each product
-		int[] distinct_purchases = new int[number_of_products];
 		
-		//prepopulates number_purchased and distinct_purchases to avoid weird nonsense
-		for(int h = 0; h < number_of_products; h++) {
-			number_purchases[h] = 0;
-			distinct_purchases[h] = 0;
-		}
-		
-		//this loop will add each product name to the array
-		for(int i = 0; i<number_of_products; i++) {
-			
-			//reads the product name from the input
-			String product_name = scan.next();
-			
-			//reads the price of each product
-			//this information is not used in this program
+		//reads names of products into the array
+		for(int i = 0; i < number_of_products; i++) {
+			//reads each product name
+			product_names[i] = scan.next();
+			//skips the price of each product
 			scan.nextDouble();
-			
-			//adds the product name into the array
-			product_names[i] = product_name;
 		}
 		
-		//reads the number of customers from the input
+		//array for holding the number of each product sold, indexed to product_names
+		int[] number_sold = new int[number_of_products];
+		//array for holding the distinct instances of each product being sold, indexed to product_names
+		int[] distinct_sold = new int[number_of_products];
+		
+		//reads the total number of customers
 		int number_of_customers = scan.nextInt();
 		
-		//this loop cycles through each customer and determines how many of each product they purchased
 		for(int j = 0; j < number_of_customers; j++) {
-			
-			//reads the customer's name
-			//this information is not used in this program
+			//skips the name of each customer
 			scan.next();
 			scan.next();
 			
-			//reads the number of products the customer purchased
-			int number_customer_purchased_total = scan.nextInt();
+			//reads the number of sale entries this customer has
+			int number_of_entries = scan.nextInt();
 			
-			//cycles through each product the customer purchased
-			for(int k = 0; k < number_customer_purchased_total; k++) {
-				
-				//reads the number of this product the customer purchased
-				int number_customer_purchased = scan.nextInt();
-				
-				//reads the name of the product the customer purchased
-				String product_name_customer_purchased = scan.next();
-				
-				//finds the appropriate index for each product in the output arrays and increments the values in those arrays
-				for(int l = 0; l < number_of_products; l++) {
-					if(product_names[l].equals(product_name_customer_purchased)) {
-						number_purchases[l] += number_customer_purchased;
-						distinct_purchases[l]++;
-						break;
-					}
+			//array holding quantity sold from each entry
+			int[] quantity_sold_customer = new int[number_of_entries];
+			//array holding item name from each entry
+			String[] product_name_customer = new String[number_of_entries];
+			
+			//cycles through each customer to populate quantity_sold_customer and product_name_customer
+			for(int k = 0; k < number_of_entries; k++) {
+				//reads quantity sold for each entry
+				quantity_sold_customer[k] = scan.nextInt();
+				//reads product name for each entry
+				product_name_customer[k] = scan.next();
+			}
+			
+			//cycles through quantity_sold_customer and product_name_customer to add to number_sold
+			for(int l = 0; l < number_of_entries; l++) {
+				//finds the location in the product_names array where each product appears
+				int name_index = findInArray(product_name_customer[l], product_names);
+				number_sold[name_index] += quantity_sold_customer[l];
+			}
+			
+			//removes duplicates from the list of products
+			product_name_customer = removeDuplicates(product_name_customer);
+			
+			//cycles through product_name_customer to add to distinct_sold
+			for(int m = 0; m < product_name_customer.length; m++) {
+				//finds the location in the product_names array where each product appears
+				int name_index = findInArray(product_name_customer[m], product_names);
+				if(name_index!=-1)
+					distinct_sold[name_index]++;
+			}
+		}
+		
+		//prints the inputs for each product using product_names, number_sold, and distinct_sold
+		for(int n = 0; n < product_names.length; n++) {
+			if(number_sold[n]==0) {
+				System.out.println("No customers bought " + product_names[0]);
+			} else {
+				System.out.println(distinct_sold[n] + " customers bought " + number_sold[n] + " " + product_names[n]);
+			}
+		}
+		
+		scan.close();
+	}
+	
+	//helper method to find a specific string in an array
+	//returns the first index where the specified string appears
+	//if the string does not appear, returns -1
+	private static int findInArray(String search_for, String[] search_in) {		
+		for(int i = 0; i < search_in.length; i++) {
+			if(search_for.equals(search_in[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//helper method to remove duplicates from a string array and replace them with "dummy"
+	private static String[] removeDuplicates(String[] input_array) {
+		for(int i = 1; i < input_array.length; i++) {
+			for(int j = 0; j < i; j++) {
+				if(input_array[i].equals(input_array[j])) {
+					input_array[j] = "dummy";
 				}
 			}
 		}
-		
-		//this array contains strings for each output value
-		String[] output_strings = new String[number_of_products];
-		
-		//cycles through each product to generate an appropriate string for output
-		for(int m = 0; m < output_strings.length; m++) {
-			if(number_purchases[m]==0) {
-				output_strings[m] = "No customers bought " + product_names[m];
-			} else {
-				output_strings[m] = distinct_purchases[m] + " customers bought " + number_purchases[m] + " " + product_names[m];
-			}
-		}
-		
-		//outputs each index of the output array on its own line
-		for(int n = 0; n < output_strings.length; n++) 
-			System.out.println(output_strings[n]);
-		
-		scan.close();
+		return input_array;
 	}
 }
